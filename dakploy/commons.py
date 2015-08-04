aebe78a7cd4591af0b5c2f43c3417b57330ed1ca
Brice Carpentier <brice@daknet.org>
2015-08-04 13:29:47 +0100

    moved setup_push_to_deploy to git and added activate/deactivate

--- dakploy/commons.py
+++ dakploy/commons.py
@@ -1,37 +1,8 @@
-import os
-
 from StringIO import StringIO
 
 from contextlib import contextmanager
 
 from fabric.api import *
-from fabric.contrib.files import exists, upload_template
-
-from fabtools import require
-
-
-@task
-def setup_push_to_deploy(repository, deploy_script):
-    require.git.command()
-
-    if not exists(repository):
-        run('mkdir -p {}'.format(repository))
-
-        with cd(repository):
-            run('git init --bare')
-
-    post_receive_hook_path = os.path.join(
-        repository, 'hooks', 'post-receive')
-
-    if isinstance(deploy_script, StringIO):
-        put(deploy_script,
-            post_receive_hook_path,
-            mode='0755')
-    else:
-        upload_template(deploy_script,
-                        post_receive_hook_path,
-                        context=env,
-                        mode='0755')
 
 
 @task
--- dakploy/git.py
+++ dakploy/git.py
@@ -1,7 +1,56 @@
-from fabric.api import settings, hide, local
+import os.path
+from StringIO import StringIO
+
+
+from fabric.api import *
+from fabric.contrib.files import (
+    comment,
+    exists,
+    uncomment,
+    upload_template,
+)
+from fabtools import require
 
 
 def get_current_branch():
     with settings(hide('running')):
         return local('git symbolic-ref HEAD',
                      capture=True).split('/')[-1]
+
+
+@task
+def setup_push_to_deploy(repository, deploy_script):
+    require.git.command()
+
+    if not exists(repository):
+        run('mkdir -p {}'.format(repository))
+
+        with cd(repository):
+            run('git init --bare')
+
+    post_receive_hook_path = os.path.join(
+        repository, 'hooks', 'post-receive')
+
+    if isinstance(deploy_script, StringIO):
+        put(deploy_script,
+            post_receive_hook_path,
+            mode='0755')
+    else:
+        upload_template(deploy_script,
+                        post_receive_hook_path,
+                        context=env,
+                        mode='0755')
+
+
+@task
+def deactivate_push_to_deploy(repository):
+    post_receive_hook_path = os.path.join(
+        repository, 'hooks', 'post-receive')
+    comment(post_receive_hook_path, r'^.*$')
+
+
+@task
+def activate_push_to_deploy(repository):
+    post_receive_hook_path = os.path.join(
+        repository, 'hooks', 'post-receive')
+    uncomment(post_receive_hook_path, r'^.*$')
